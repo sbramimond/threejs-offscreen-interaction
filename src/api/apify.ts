263137a7b73c7106812f5409f139f804fa589f53
neverland <enix@foxmail.com>
2025-12-02 16:22:35 +0800

    feat: add websocket support

--- src/api/apify.ts
+++ src/api/apify.ts
@@ -1,14 +1,15 @@
 import getOption from './getOption';
+import axios, { Axios, type AxiosRequestConfig }  from 'axios';  
 
-export default function apify(axios, method, list = {}) {
+export default function apify(i: Axios, method: string, list = {}) {
     let all = Object.keys(list);
     let result = {};
 
     all.forEach((key) => {
         let url = list[key];
 
-        result[key] = (...parameter) => {
-            return axios(getOption(url, method.toUpperCase(), ...parameter));
+        result[key] = (...parameter: any) => {
+            return i(getOption(url, method.toUpperCase(), ...parameter) as AxiosRequestConfig);
         };
     });
 
